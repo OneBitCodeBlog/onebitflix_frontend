225aa832ade1955895e8fe9d8363448369b921aa
Lucas <lucasqueirogadev@gmail.com>
2022-06-16 12:47:34 -0300

    Criação do header da página homeNoAUth - Header não autenticado

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,22 @@
 import Head from "next/head";
+import HeaderNoAuth from "../src/components/homeNoAuth/headerNoAuth";
 import styles from "../styles/HomeNoAuth.module.scss";
 
 const HomeNoAuth = () => {
   return (
     <>
       <Head>
-        <title>Home1</title>
+        <title>Onebitflix</title>
+        <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
+        <meta property="og:title" content="Onebitflix" key="title" />
+        <meta
+          name="description"
+          content="Tenha acesso aos melhors conteúdos de programação de uma forma simples e fácil!"
+        />
       </Head>
-      <main></main>
+      <main>
+        <HeaderNoAuth />
+      </main>
     </>
   );
 };
