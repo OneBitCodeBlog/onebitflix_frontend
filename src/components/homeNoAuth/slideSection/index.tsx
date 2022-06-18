c170bbd6f28ce37fc088ea6102e91ee069333eb7
Lucas <lucasqueirogadev@gmail.com>
2022-06-18 12:12:25 -0300

    Finalização da estrutura dos slides - Slide Section

---
+++ src/components/homeNoAuth/slideSection/index.tsx
@@ -0,0 +1,27 @@
+import styles from "./styles.module.scss";
+import { Button, Container } from "reactstrap";
+import { CourseType } from "../../../services/courseService";
+import SlideComponent from "../../common/slideComponent";
+import Link from "next/link";
+
+interface props {
+  newestCourses: CourseType[];
+}
+
+const SlideSection = function ({ newestCourses }: props) {
+  return (
+    <>
+      <Container>
+        <p className={styles.sectionTitle}>AULAS JÁ DISPONÍVEIS</p>
+        <SlideComponent course={newestCourses} />
+        <Link href="/register">
+          <Button outline color="light" className={styles.slideSectionBtn}>
+            Se cadastre para acessar!
+          </Button>
+        </Link>
+      </Container>
+    </>
+  );
+};
+
+export default SlideSection;
