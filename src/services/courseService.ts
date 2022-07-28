8796bcb5e0d171284661b48c67d46dc11321f4e9
Lucas <lucasqueirogadev@gmail.com>
2022-07-27 21:36:20 -0300

    Criação dos elementos da seção de favorites, do seu estilo visual e também dos métodos de favorito (Add &  Delete) - Seção favoritos

--- src/services/courseService.ts
+++ src/services/courseService.ts
@@ -20,8 +20,6 @@ export type CourseType = {
 const courseService = {
   getNewestCourses: async () => {
     const res = await api.get("/courses/newest").catch((error) => {
-      console.log(error.response.data.message);
-
       return error.response;
     });
 
@@ -37,8 +35,54 @@ const courseService = {
         },
       })
       .catch((error) => {
-        console.log(error.response.data.message);
+        return error.response;
+      });
 
+    return res;
+  },
+  addToFav: async (courseId: number | string) => {
+    const token = sessionStorage.getItem("onebitflix-token");
+
+    const res = await api
+      .post(
+        "/favorites",
+        { courseId },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      )
+      .catch((error) => {
+        return error.response;
+      });
+
+    return res;
+  },
+  removeFav: async (courseId: number | string) => {
+    const token = sessionStorage.getItem("onebitflix-token");
+
+    const res = await api
+      .delete("/favorites", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+        data: { courseId },
+      })
+      .catch((error) => {
+        return error.response;
+      });
+
+    return res;
+  },
+  getFavCourses: async () => {
+    const token = sessionStorage.getItem("onebitflix-token");
+
+    const res = await api
+      .get("/favorites", {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      .catch((error) => {
         return error.response;
       });
 
@@ -47,5 +91,3 @@ const courseService = {
 };
 
 export default courseService;
-
-const teste2 = "";
