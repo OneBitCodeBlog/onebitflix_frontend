6363922af40a8c55ad6c3a06cf2e1a1aba324e26
Lucas <lucasqueirogadev@gmail.com>
2022-07-29 16:40:06 -0300

    Conexão do backend relacionado ao update de senha. Criação do método e também de toda a configuração de conexão com o front - Profile (Password - Conexão com o backend)

--- src/components/profile/password/index.tsx
+++ src/components/profile/password/index.tsx
@@ -1,10 +1,82 @@
+import { FormEvent, useEffect, useState } from "react";
 import { Button, Form, FormGroup, Input, Label } from "reactstrap";
 import styles from "../../../../styles/profile.module.scss";
+import profileService from "../../../services/profileService";
+import ToastComponent from "../../common/toast";
 
 const PasswordForm = function () {
+  const [currentPassword, setCurrentPassword] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+  const [confirmNewPassword, setConfirmNewPassword] = useState("");
+  const [color, setColor] = useState("");
+  const [toastIsOpen, setToastIsOpen] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  useEffect(() => {
+    profileService.fetchCurrent().then((password) => {
+      setCurrentPassword(password.currentPassword);
+      setNewPassword(password.newPassword);
+    });
+  }, []);
+
+  const handlePasswordUpdate = async function (
+    event: FormEvent<HTMLFormElement>
+  ) {
+    event.preventDefault();
+
+    if (newPassword != confirmNewPassword) {
+      setToastIsOpen(true);
+      setErrorMessage("Senha e confirmação de senha diferentes!");
+      setColor("bg-danger");
+      setTimeout(() => {
+        setToastIsOpen(false);
+      }, 1000 * 3);
+
+      return;
+    }
+
+    if (currentPassword === newPassword) {
+      setToastIsOpen(true);
+      setErrorMessage("Não coloque a nova senha iguakl a senha antiga!");
+      setColor("bg-danger");
+      setTimeout(() => {
+        setToastIsOpen(false);
+      }, 1000 * 3);
+
+      return;
+    }
+
+    const res = await profileService.passwordUpdate({
+      currentPassword,
+      newPassword,
+    });
+
+    if (res === 204) {
+      setToastIsOpen(true);
+      setErrorMessage("Senha alterada com sucesso!");
+      setColor("bg-success");
+      setTimeout(() => {
+        setToastIsOpen(false);
+      }, 1000 * 3);
+
+      setCurrentPassword("");
+      setNewPassword("");
+      setConfirmNewPassword("");
+    }
+
+    if (res === 400) {
+      setToastIsOpen(true);
+      setErrorMessage("Senha atual incorreta!");
+      setColor("bg-danger");
+      setTimeout(() => {
+        setToastIsOpen(false);
+      }, 1000 * 3);
+    }
+  };
+
   return (
     <>
-      <Form className={styles.form}>
+      <Form onSubmit={handlePasswordUpdate} className={styles.form}>
         <div className={styles.inputNormalDiv}>
           <FormGroup>
             <Label className={styles.label} for="currentPassword">
@@ -18,6 +90,10 @@ const PasswordForm = function () {
               required
               minLength={6}
               maxLength={12}
+              value={currentPassword}
+              onChange={(event) => {
+                setCurrentPassword(event.currentTarget.value);
+              }}
               className={styles.input}
             />
           </FormGroup>
@@ -35,6 +111,10 @@ const PasswordForm = function () {
               required
               minLength={6}
               maxLength={12}
+              value={newPassword}
+              onChange={(event) => {
+                setNewPassword(event.currentTarget.value);
+              }}
               className={styles.inputFlex}
             />
           </FormGroup>
@@ -50,14 +130,23 @@ const PasswordForm = function () {
               required
               minLength={6}
               maxLength={12}
+              value={confirmNewPassword}
+              onChange={(event) => {
+                setConfirmNewPassword(event.currentTarget.value);
+              }}
               className={styles.inputFlex}
             />
           </FormGroup>
         </div>
-        <Button className={styles.formBtn} outline>
+        <Button type="submit" className={styles.formBtn} outline>
           Salvar Alterações
         </Button>
       </Form>
+      <ToastComponent
+        color={color}
+        isOpen={toastIsOpen}
+        message={errorMessage}
+      />
     </>
   );
 };
